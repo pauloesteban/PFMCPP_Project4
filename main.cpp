5acd797cf1d63b5924192b1af9d364aca41635cb
Paulo Esteban Chiliguano Torres <paulo.chiliguano@gmail.com>
2020-06-30 04:34:20 +0000

    Extra decimal place

--- main.cpp
+++ main.cpp
@@ -110,7 +110,7 @@ double DoubleType::multiply( double lhs, double rhs )
 
 double DoubleType::divide( double lhs, double rhs )
 {
-    if (rhs == 0.0)
+    if (rhs == 0.00)
     {
         std::cout << std::endl << "warning, floating point division by zero returns 'inf' !" << std::endl;
     }
