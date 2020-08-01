ae4ab8d86b7309a9f357bb4e23950f607d7be06b
Paulo Esteban Chiliguano Torres <paulo.chiliguano@gmail.com>
2020-08-01 16:16:46 +0000

    Step 0

--- main.cpp
+++ main.cpp
@@ -14,15 +14,18 @@ New/This/Pointers/References conclusion
          on the heap without leaking, without using smart pointers. 
  */
 
+struct A{};
 
-
-
-
-
-
-
-
-
+struct HeapA
+{
+    A* ptrA;
+    HeapA() : ptrA( new A() ){}
+    ~HeapA()
+    {
+        delete ptrA;
+        ptrA = nullptr;
+    }
+};
 
  /*
  1) Edit your 3 structs so that they own a heap-allocated primitive type without using smart pointers named 'value'
